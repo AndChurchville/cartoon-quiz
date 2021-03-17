19259bc984a810cbf59b760f9037be9924e2c7e9
AndChurchville <48935849+AndChurchville@users.noreply.github.com>
2021-03-17 17:36:05 -0500

    start with small html css changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=32&type=multiple';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='bg-white text-blue-800 p-10 rounded-lg shadow-md '>
+          <h2 className='text-2xl '>This is a Question</h2>
+          </div>
+
+        <div>
+          <div>Answer 1</div>
+          <div>Answer 2</div>
+          <div>Answer 3</div>
+          <div>Answer 4</div>
+        </div>
+      </div>
+    </>
   );
 }
 
